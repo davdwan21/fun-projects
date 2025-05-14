70d8b0b3d0f4b2c8d6eaa518cdab0d7dc7686c30
davdwan21 <141950513+davdwan21@users.noreply.github.com>
2025-05-14 10:43:13 -0700

    why is this a commit

--- testing.py
+++ testing.py
@@ -1,4 +1,5 @@
 import keyboard  # using module keyboard
+
 while True:  # making a loop
     try:  # used try so that if user pressed other than the given key error will not be shown
         if keyboard.is_pressed('q'):  # if key 'q' is pressed 
